04f3ac00877e0dbba590df574479f9b014e001d3
Adam Snaider <adsnaider@gmail.com>
2024-11-17 17:26:25 -0500

    Add support for tuple structs

--- burrow/tests/derives.rs
+++ burrow/tests/derives.rs
@@ -3,26 +3,21 @@ use std::borrow::Cow;
 use burrow::Burrow;
 
 #[derive(Burrow)]
-pub struct Foo<'a> {
-    nothing: Cow<'a, str>,
-    foo: usize,
-    bar: String,
-}
-
-#[derive(Burrow)]
-pub struct Bar<'a, 'b> {
+pub struct Foo<'a, 'b> {
     nothinga: Cow<'a, str>,
     nothingb: Cow<'b, str>,
     foo: usize,
     bar: String,
 }
 
-#[allow(dead_code)]
-fn it_works_foo(f: Foo<'_>) -> Foo<'static> {
-    f.to_static()
-}
+#[derive(Burrow)]
+pub struct Bar<'a, 'b>(Cow<'a, str>, Cow<'b, str>, usize, String);
 
 #[allow(dead_code)]
-fn it_works_bar(b: Bar<'_, '_>) -> Bar<'static, 'static> {
-    b.to_static()
+fn it_works_1(b: Foo<'_, '_>) -> Foo<'static, 'static> {
+    b.into_static()
+}
+#[allow(dead_code)]
+fn it_works_2(b: Bar<'_, '_>) -> Bar<'static, 'static> {
+    b.into_static()
 }
--- burrow_derive/src/lib.rs
+++ burrow_derive/src/lib.rs
@@ -1,6 +1,6 @@
 use proc_macro2::{Ident, TokenStream};
 use quote::{quote, ToTokens};
-use syn::{parse_macro_input, ConstParam, DeriveInput, TypeParam};
+use syn::{parse_macro_input, ConstParam, DataStruct, DeriveInput, TypeParam};
 
 #[proc_macro_derive(Burrow)]
 pub fn derive_burrow(item: proc_macro::TokenStream) -> proc_macro::TokenStream {
@@ -8,22 +8,35 @@ pub fn derive_burrow(item: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let lifetimes: Vec<_> = item.generics.lifetimes().collect();
 
     let name = item.ident;
-    let syn::Data::Struct(strct) = item.data else {
-        unimplemented!("Only structs are supported");
-    };
-    let syn::Fields::Named(fields) = strct.fields else {
-        unimplemented!("Only named structs are supported");
-    };
-
-    let fields = fields.named.iter().map(|f| f.ident.as_ref().unwrap());
-    generate_for_named_struct(
-        &name,
-        lifetimes.len(),
-        &item.generics.type_params().collect::<Vec<_>>(),
-        &item.generics.const_params().collect::<Vec<_>>(),
-        &fields.collect::<Vec<_>>(),
-    )
-    .into()
+    match item.data {
+        syn::Data::Struct(DataStruct { fields, .. }) => match fields {
+            syn::Fields::Named(fields) => {
+                let fields = fields.named.iter().map(|f| f.ident.as_ref().unwrap());
+                generate_for_named_struct(
+                    &name,
+                    lifetimes.len(),
+                    &item.generics.type_params().collect::<Vec<_>>(),
+                    &item.generics.const_params().collect::<Vec<_>>(),
+                    &fields.collect::<Vec<_>>(),
+                )
+                .into()
+            }
+            syn::Fields::Unnamed(fields) => {
+                let fields = fields.unnamed.len();
+                generate_for_tuple_struct(
+                    &name,
+                    lifetimes.len(),
+                    &item.generics.type_params().collect::<Vec<_>>(),
+                    &item.generics.const_params().collect::<Vec<_>>(),
+                    fields,
+                )
+                .into()
+            }
+            syn::Fields::Unit => todo!(),
+        },
+        syn::Data::Enum(enm) => todo!(),
+        syn::Data::Union(_) => panic!("Burrow may not be implemented for `union` types"),
+    }
 }
 
 #[derive(Debug, Copy, Clone)]
@@ -55,6 +68,32 @@ fn generate_for_named_struct(
                 #name {
                     #(#fields: self.#fields.into_static(),)*
                 }
+            }
+        }
+    }
+}
+
+fn generate_for_tuple_struct(
+    name: &Ident,
+    lifetimes: usize,
+    generics: &[&TypeParam],
+    const_params: &[&ConstParam],
+    fields: usize,
+) -> TokenStream {
+    let life: Vec<_> = std::iter::repeat(Void).take(lifetimes).collect();
+    if !const_params.is_empty() {
+        unimplemented!("const params are not yet supported");
+    }
+    let fields = (0..fields).map(syn::Index::from);
+    quote! {
+        impl ::burrow::Burrow for #name<#(#life '_,)*#(#generics,)*> {
+            type OwnedSelf = #name<#(#life 'static,)*#(#generics,)*>;
+
+            fn into_static(self) -> Self::OwnedSelf {
+                use ::burrow::Burrow;
+                #name (
+                    #(self.#fields.into_static(),)*
+                )
             }
         }
     }
